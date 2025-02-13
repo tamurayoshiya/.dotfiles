ca29670a1dee15d2ee5c177d55b316b6898f5662
TAMURA Yoshiya <a@qmu.jp>
2025-02-14 00:05:21 +0900

    update

--- files/.vim/lua/kickstart.lua
+++ files/.vim/lua/kickstart.lua
@@ -447,7 +447,11 @@ require("lazy").setup({
 				-- python = { "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			},
 		},
 	},
