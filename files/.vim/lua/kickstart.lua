b99d0e4a21ab604f51fbc3647b26fc57a58b3195
TAMURA Yoshiya <a@qmu.jp>
2025-02-28 00:51:14 +0900

    update

--- files/.vim/lua/kickstart.lua
+++ files/.vim/lua/kickstart.lua
@@ -756,6 +756,29 @@ require("lazy").setup({
 			require("gitsigns").setup()
 		end,
 	},
+
+	-- Copilot Chat
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		keys = {
+			{
+				"<leader>cc",
+				function()
+					return require("CopilotChat").toggle()
+				end,
+				desc = "[P]Toggle (CopilotChat)",
+				mode = { "n", "i", "v" },
+			},
+		},
+	},
 })
 
 -- スペース2回でホバー（シグネチャ表示）
