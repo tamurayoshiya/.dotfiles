7214c9c4ee15bc0741ec768a6cac97c9d8c5d6ea
TAMURA Yoshiya <a@qmu.jp>
2025-04-23 18:08:11 +0900

    update

--- files/.vim/lua/kickstart.lua
+++ files/.vim/lua/kickstart.lua
@@ -100,11 +100,17 @@ require("lazy").setup({
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
+				defaults = {
+					mappings = {
+						i = { 
+							['<c-enter>'] = 'to_fuzzy_refine',
+							['<C-p>'] = require('telescope.actions').cycle_history_prev,
+							['<C-n>'] = require('telescope.actions').cycle_history_next,
+							['<Up>'] = require('telescope.actions').cycle_history_prev,
+							['<Down>'] = require('telescope.actions').cycle_history_next,
+						},
+					},
+				},
 				-- pickers = {}
 				extensions = {
 					["ui-select"] = {
